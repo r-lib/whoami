e602b77966c0866b868f576f096d9f997e0cfdcb
Gabor Csardi <csardi.gabor@gmail.com>
2015-06-11 09:41:18 -0400

    Better test for net connection in gh_username test
    Also, skip this test on CRAN, so be absolutely safe.

--- tests/testthat/test-gh-username.R
+++ tests/testthat/test-gh-username.R
@@ -3,18 +3,26 @@ context("GitHub username")
 
 test_that("Github username works", {
 
-  try(gh <- httr::GET(
-    "https://api.github.com",
-    httr::add_headers("user-agent" = "https://github.com/gaborcsardi/whoami"),
-    httr::timeout(1.0)
-  ))
+  skip_on_cran()
+  
+  tr <- try(
+    silent = TRUE,
+    gh <- httr::GET(
+      "https://api.github.com",
+      httr::add_headers("user-agent" = "https://github.com/gaborcsardi/whoami"),
+      httr::timeout(1.0)
+    )
+  )
 
-  if (gh$status_code != 200) skip("No internet, skipping")
+  if (inherits(tr, "try-error") || gh$status_code != 200) {
+    skip("No internet, skipping")
+  }
 
   with_mock(
     `whoami::email_address` = function(...) "csardi.gabor@gmail.com",
     ghuser <- gh_username()
   )
+
 
   expect_equal(ghuser, "gaborcsardi")
   
