759d54ea68c4bf68b4ff393720428a44ac807dae
--show-origin <maelle.salmon@yahoo.se>
2018-07-12 12:10:43 +0200

    import getFromNamespace

--- R/whoami.R
+++ R/whoami.R
@@ -219,7 +219,7 @@ email_address <- function(fallback = NULL) {
 #' @export
 #' @importFrom httr GET add_headers status_code content
 #' @importFrom jsonlite fromJSON
-#' @importFrom utils URLencode assignInMyNamespace
+#' @importFrom utils URLencode assignInMyNamespace getFromNamespace
 #' @examples
 #' \dontrun{
 #' gh_username()
